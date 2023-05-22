b9903c562e5cc67d9bcbfef3050b685d375f5311
Семененко Надежда <tn@impsa.ru>
2023-05-22 23:45:05 +0300

    add notifications from fake api

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postReducer from '../features/posts/postSlice'
 import useReducer from '../features/users/usersSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     users: useReducer,
+    notifications: notificationsReducer,
   },
 })
